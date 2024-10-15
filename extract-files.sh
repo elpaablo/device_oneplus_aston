d570f86c8bc242392aaa45b25488eeb3ba87c682
inferno0230 <mail@inferno0230.in>
2024-10-15 23:19:47 +0530

    salami: Move some parts from common
    Following adding support for aston
    
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,28 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        odm/etc/camera/CameraHWConfiguration.config)
+            [ "$2" = "" ] && return 0
+            sed -i "/SystemCamera = / s/1;/0;/g" "${2}"
+            ;;
+        odm/lib64/libAlgoProcess.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s/android.hardware.graphics.common-V3-ndk.so/android.hardware.graphics.common-V5-ndk.so/" "${2}"
+            ;;
+        odm/lib64/libCOppLceTonemapAPI.so|odm/lib64/libCS.so|odm/lib64/libSuperRaw.so|odm/lib64/libYTCommon.so|odm/lib64/libyuv2.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
