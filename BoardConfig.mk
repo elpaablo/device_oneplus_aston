1d06ba2835953862429abdfb50751c935ac853b9
Aditya Choudhury <mail@inferno0230.in>
2024-10-16 14:32:28 +0530

    aston: Override dynamic and super partition size in recovery while sideloading: [ERROR:builder.cpp(1047)] [liblp] Device size does not match (got 16642998272, expected 16940199936)
    Value from aston: `(bootloader) partition-size:super:0x3E0000000` => 16642998272
    
    so set it accordingly.
    
    Signed-off-by: Aditya Choudhury <mail@inferno0230.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,10 @@ BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVIC
 BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery $(DEVICE_PATH)/modules.include.vendor_ramdisk))
 SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.include.system_dlkm))
 
+# Partitions
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 16638803968
+BOARD_SUPER_PARTITION_SIZE := 16642998272
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
